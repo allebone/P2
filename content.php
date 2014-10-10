4f12ec463f590449728c875d93a4f0a17b97fe06
Bryan Allebone <bryanallebone@g.harvard.edu>
2014-10-10 16:21:14 -0500

    fixes

--- content.php
+++ content.php
@@ -1,9 +1,9 @@
         <div class="row marketing">
-            <div class="col-lg-6" id="about">
+            <div class="col-lg-12" id="about">
                 <h4>About</h4>
 
 
-                <p><h3>XKCD passwords (as demonstrated in the comic at:http://xkcd.com/936/) is a method to maintain an easy to remember password that also meets most complexity requirements. It's core/fundamental means is by generating unrelated words from a wordlist (or database) that increase length while giving the option of including Special Characters, Numbers, or alternate cases.  These combined efforts produce a password of entropy (length) long enough to deride most attempts at Brute Force attack.</h3></p>
+                <p>XKCD passwords (as demonstrated in the comic at: <a href="http://xkcd.com/936/">http://xkcd.com/936/</a> is a method to maintain an easy to remember password that also meets most complexity requirements. It's core/fundamental means is by generating unrelated words from a wordlist (or database) that increase length while giving the option of including Special Characters, Numbers, or alternate cases.  These combined efforts produce a password of entropy (length) long enough to deride most attempts at Brute Force attack.</p>
 
 
                 <h4>Demo</h4>
@@ -16,24 +16,22 @@
         <div class="jumbotron" id="generator">
             <h4>XKCD Password Generator</h4>
 
-            <p>
             <div class="bs-example">
-                <form action=<?=basename($_SERVER['PHP_SELF'])?>" method="POST">
+                <form action="<?php basename($_SERVER['PHP_SELF']) ?>" method="POST">
                         <div class="row">
                             <div class="col-md-12">
-                                <input class="form-control input-lg" placeholder="Click Generate to Create Password" type="text" value="<?php printPass(); ?>"
-                                readonly>
+                                <input class="form-control input-lg" placeholder="Click Generate to Create Password" type="text" value="" readonly>
                             </div>
                         </div>
                         <div class="row">
                             <div class="col-md-6">                    
                                 <h5>Number of Words: </h5>
                                   <input name="numWords" class="form-control input-lg"
-					type="number"
-				        min="0"
-				        max="10"
-				        step="1"
-         				value="5">
+					                           type="number"
+				                              min="0"
+				                              max="10"
+				                              step="1"
+         				                      value="5">
                             </div>   
                             
                             <div class="col-md-6"> 
