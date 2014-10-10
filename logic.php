2f04490063c5860c6bad42324c22e470578aa220
Bryan Allebone <bryan.allebone@yahoo.com>
2014-10-10 14:55:59 -0500

    Update logic.php

--- logic.php
+++ logic.php
@@ -1,53 +1,99 @@
 <?php
-if (isset ( $_POST ['numWords'] )) {
-	$numWords = $_POST ['numWords'];
-}
-
-if (isset ( $_POST ['specChar'] )) {
-	$specChar = $_POST ['specChar'];
-}
-
-if (isset ( $_POST ['caseChoice'] )) {
-	$caseChoice = $_POST ['caseChoice'];
-}
-
-if (isset ( $_POST ['number'] )) {
-	$number = $_POST ['number'];
-}
-
-if ($words = file ( './wordlist.txt' )) {
-	$selected_words = [ ];
 	
-	for($i = 0; $i < $numWords; $i ++) {
-		$max = count ( $words ) - 1;
-		$rand = rand ( 0, $max );
-		$word = $words [$rand];
-		array_push ( $selected_words, $word );
-	}
-}
-
-// echo $caseChoice;
-
-if ($caseChoice) {
 	
-	foreach ( $selected_words as $key => $value ) {
-		if ($caseChoice == 'lower') {
-			$selected_words [$key] = strtolower ( $value );
-		} else {
-			foreach ( $selected_words as $key => $value ) {
-				if ($caseChoice == 'UPPER') {
-					$selected_words [$key] = strtoupper ( $value );
-				} else {
-					foreach ( $selected_words as $key => $value ) {
-						$selected_words [$key] = ucfirst ( $value );
-					}
-				}
-			}
+	
+	function printPass() {
+
+	$numWords;
+	$specChar;
+	$caseChoice;
+	$addNum;
+	
+		if (isset ( $_POST ['numWords'] )) {
+			$numWords = $_POST ['numWords'];
 		}
+
+		
+		if (isset ( $_POST ['specChar'] )) {
+			$specChar = $_POST ['specChar'];
+		}
+
+	
+		if (isset ( $_POST ['caseChoice'] )) {
+			$caseChoice = $_POST ['caseChoice'];
+		}
+
+	
+		if (isset ( $_POST ['addNum'] )) {
+			$number = $_POST ['addNum'];
+		}
+
+		echo .generatePass($numWords, $addNum, $specChar, $caseChoice);
+
 	}
-}
 
-echo "<br> answer: ";
-print_r ( $selected_words );
 
-?>
+	function generatePass($numWords, $addNum, $specChar, $caseChoice) {
+
+
+
+
+	if ($words = file ( './wordlist.txt' )) {
+		$selectedWords = [ ];
+		for($i = 0; $i < $numWords; $i ++) {
+			$max = count ( $words ) - 1;
+			$rand = rand ( 0, $max );
+			$word = $words [$rand];
+			array_push ( $selectedWords, $word );
+		}
+
+	}
+	
+	if ($caseChoice) {
+		foreach ( $selectedWords as $key => $value ) {
+			
+			if ($caseChoice == 'lower') {
+				$selectedWords [$key] = strtolower ( $value );
+			} else {
+				foreach ( $selectedWords as $key => $value ) {
+					
+					if ($caseChoice == 'UPPER') {
+						$selectedWords [$key] = strtoupper ( $value );
+					} else {
+						foreach ( $selectedWords as $key => $value ) {
+							$selectedWords [$key] = ucfirst ( $value );
+						}
+
+					}
+
+				}
+
+			}
+
+		}
+
+	}
+
+	
+	function make_password($number_of_words, $has_number, $has_symbol) {
+
+		$thepassword = "";
+		for ($i = 0; $i < $numWords; $i++) {
+			$newword=$words[rand(0, (sizeof($words)-1))];
+			$thepassword = $thepassword.$newword;
+		}
+
+		
+		if ($has_number) {
+			$thepassword = $thepassword.rand(0,9);
+		}
+
+		
+		if ($has_symbol) {
+			$thepassword = $thepassword.$symbols[rand(0, (sizeof($symbols)-1))];
+		}
+
+		return $thepassword;
+	}
+
+	?>
